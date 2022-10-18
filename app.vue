fdcea49d6e5b1d6364eeac86b8efe494d71dfe59
muranuun <mura3-2033@hotmail.co.jp>
2022-10-18 20:02:34 +0900

    fixed max height and added custom scroll bar

--- app.vue
+++ app.vue
@@ -1,32 +1,30 @@
 <template>
   <div class="dark flex flex-col items-center justify-center min-h-screen dark:bg-slate-900 dark:text-slate-300 font-digital">
-    <div class="rounded-xl py-4 px-8 lg:py-8 lg:px-12 lg:shadow-lg lg:shadow-slate-600">
-      <div class="divide-y divide-slate-700 space-y-" v-if="!loading">
-        <div class="grid grid-cols-2 grid-rows-2 gap-4">
-          <h1 class="row-span-2 text-6xl text-slate-100" style="text-shadow: 2px -2px 5px #334155;">{{ formatDuration(stats.todaysQuantity) }}</h1>
-          <div class="row-start-2 col-start-2 ml-auto text-right text-slate-400 text-sm">
-            <p>Total work: {{ stats.totalPixelsCount }} days</p>
-            <p>Daily average work: {{ formatDuration(stats.avgQuantity) }}</p>
-          </div>
-        </div>
-        <div>
-          <h2 class="text-3xl my-4">History</h2>
-          <div v-for="h in history" :key="h.date" class="ml-4 text-slate-400">
-            <div class="flex flex-row items-end gap-2 border-b border-b-slate-700">
-              <h3 class="text-xl">{{ formatDate(h.date) }}</h3>
-              <p class="text-sm ml-auto">Total: {{ formatDuration(h.quantity) }}</p>
-            </div>
-            <ul>
-              <li v-for="(p, index) in JSON.parse(h.optionalData).stopwatchUsage.stopwatchPeriods" :key="h.date + index" class="text-sm ml-8 my-2 list-disc">
-                {{ p }} ({{ calcMinutes(p) }} minutes)
-              </li>
-            </ul>
-          </div>
+    <div class="rounded-xl py-4 px-8 lg:py-8 lg:px-12 lg:shadow-lg lg:shadow-slate-600 max-h-96" v-if="!loading">
+      <div class="grid grid-cols-2 grid-rows-2 gap-4 mb-4">
+        <h1 class="row-span-2 text-6xl text-slate-100" style="text-shadow: 2px -2px 5px #334155;">{{ formatDuration(stats.todaysQuantity) }}</h1>
+        <div class="row-start-2 col-start-2 ml-auto text-right text-slate-400 text-sm">
+          <p>Total work: {{ stats.totalPixelsCount }} days</p>
+          <p>Daily average work: {{ formatDuration(stats.avgQuantity) }}</p>
         </div>
       </div>
-      <div v-else>
-        <h1 class="text-6xl text-slate-100" style="text-shadow: 2px -2px 5px #334155;">Loading...</h1>
+      <h2 class="text-3xl">History</h2>
+      <div class="my-4 max-h-40 overflow-y-scroll scroll-smooth scrollbar">
+        <div v-for="h in history" :key="h.date" class="mx-4 text-slate-400">
+          <div class="flex flex-row items-end gap-2 border-b border-b-slate-700">
+            <h3 class="text-xl">{{ formatDate(h.date) }}</h3>
+            <p class="text-sm ml-auto">Total: {{ formatDuration(h.quantity) }}</p>
+          </div>
+          <ul>
+            <li v-for="(p, index) in JSON.parse(h.optionalData).stopwatchUsage.stopwatchPeriods" :key="h.date + index" class="text-sm ml-8 my-2 list-disc">
+              {{ p }} ({{ calcMinutes(p) }} minutes)
+            </li>
+          </ul>
+        </div>
       </div>
+    </div>
+    <div class="rounded-xl py-4 px-8 lg:py-8 lg:px-12 lg:shadow-lg lg:shadow-slate-600" v-else>
+      <h1 class="text-6xl text-slate-100" style="text-shadow: 2px -2px 5px #334155;">Loading...</h1>
     </div>
   </div>
 </template>
@@ -80,3 +78,19 @@ export default {
   }
 }
 </script>
+
+<style>
+.scrollbar::-webkit-scrollbar {
+  width: 5px;
+}
+
+.scrollbar::-webkit-scrollbar-track {
+  @apply bg-slate-700;
+  border-radius: 15px;
+}
+
+.scrollbar::-webkit-scrollbar-thumb {
+  @apply bg-slate-400;
+  border-radius: 15px;
+}
+</style>
